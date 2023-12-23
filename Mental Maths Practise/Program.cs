c28ae992671c8a9c0907907d94a3f143943e7ce0
Ahmed Kehinde <ahmed.kehinde2017@gmail.com>
2023-12-23 10:39:50 +0000

    Create test class

---
+++ Mental Maths Practise/Program.cs
@@ -0,0 +1,7 @@
+﻿using Mental_Maths_Practise;
+
+
+
+Test test1 = new Test(10, 2, 3);
+test1.GenerateTest();
+test1.RunTest();
---
+++ Mental Maths Practise/Test.cs
@@ -0,0 +1,62 @@
+﻿namespace Mental_Maths_Practise;
+
+public class Test(int numberOfQuestions, int multiplierNumberOfDigits, int multiplicandNumberOfDigits)
+{
+    // Attributes
+    private int TestResult { get; set; }
+    private int[] Answers { get; } = new int[numberOfQuestions];
+    private string[] Questions { get; } = new string[numberOfQuestions];
+    private int MultiplierRange { get; } = (int)Math.Pow(10, multiplierNumberOfDigits); // stores the range of numbers the multiplier can be
+    private int MultiplicandRange { get; } = (int)Math.Pow(10, multiplicandNumberOfDigits); // stores the range of numbers the multiplicand can be
+
+    // Methods
+    public void GenerateTest()
+    {
+        Random random = new Random(); // creates an instance of the Random class
+        for (int i = 0; i < numberOfQuestions; i++)
+        {
+            int multiplicand = random.Next(1, MultiplicandRange); // generates a random number between 1 and multiplicand range
+            int multiplier = random.Next(1, MultiplierRange); // generates a random number between 1 and multiplier range
+            string question = $"{multiplicand} x {multiplier} = "; // creates the question for the user to see
+            int answer = multiplicand * multiplier; // calculates the answer
+            Questions[i] = question;
+            Answers[i] = answer;
+        }
+        
+    }
+
+    public void RunTest()
+    {
+        Console.WriteLine("----------TEST STARTED----------");
+        for (int i = 0; i < numberOfQuestions; i++)
+        {
+            Console.WriteLine("\n" + Questions[i]);
+            string? userInput = Console.ReadLine(); // gets the user's answer, accepts null as an input
+            bool isANumber = int.TryParse(userInput, out int userAnswer); // trys to parse input into an int
+            if (!isANumber) // if it can't be parsed, say the answer is wrong
+            {
+                Console.WriteLine("Incorrect");
+            }
+            else
+            {
+                if (userAnswer == Answers[i]) // if the answer is correct
+                {
+                    TestResult++; // add 1 to the score
+                    Console.WriteLine("Correct!");
+                
+                }
+                else // if the answer is wrong
+                {
+                    Console.WriteLine("Incorrect");
+                }
+            }
+            
+        }
+        Console.WriteLine("----------TEST COMPLETED----------");
+        Console.WriteLine("You scored " + TestResult + " out of " + numberOfQuestions); // outputs the score
+    }
+    
+    
+    
+    
+}
